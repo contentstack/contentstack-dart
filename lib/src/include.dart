c930801eb0a2f17eb4f51ab4320490d8e1259b0f
mshaileshr@gmail.com <Yes@383840>
2020-04-21 19:16:38 +0530

    test-cases included coverage .g.dart files excluded. include added operators.dart added syncModel to syncResult (updated) testcases added for query and entry url string changed to url uri entryQueryable include implementation done with enums updated testcases after changing url to uri

---
+++ lib/src/include.dart
@@ -0,0 +1,11 @@
+import 'package:super_enum/super_enum.dart';
+part 'include.g.dart';
+@superEnum
+enum _Include {
+@Data(fields: [DataField<List<String>>('fieldUidList')])
+None,
+@Data(fields: [DataField<List<String>>('fieldUidList')])
+Only,
+@Data(fields: [DataField<List<String>>('fieldUidList')])
+Except
+}
