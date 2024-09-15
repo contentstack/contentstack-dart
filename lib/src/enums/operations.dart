8cfdd0d3c5647de0e3d0bd420889f7e22c112480
abhinav <abhinav.gupta@contentstack.com>
2024-09-15 09:20:20 +0530

    WIP: removing superEnum dep from operations.dart

--- lib/src/enums/operations.dart
+++ lib/src/enums/operations.dart
@@ -1,26 +1,10 @@
-import 'package:super_enum/super_enum.dart';
-part 'operations.g.dart';
+import 'package:contentstack/src/enums/operations_type.dart';
 
-@superEnum
-enum _QueryOperation {
-  @Data(fields: [DataField<dynamic>('value')])
-  Equals,
-  @Data(fields: [DataField<dynamic>('value')])
-  NotEquals,
-  @Data(fields: [DataField<List<dynamic>>('value')])
-  Includes,
-  @Data(fields: [DataField<List<dynamic>>('value')])
-  Excludes,
-  @Data(fields: [DataField<dynamic>('value')])
-  IsLessThan,
-  @Data(fields: [DataField<dynamic>('value')])
-  IsLessThanOrEqual,
-  @Data(fields: [DataField<dynamic>('value')])
-  IsGreaterThan,
-  @Data(fields: [DataField<dynamic>('value')])
-  IsGreaterThanOrEqual,
-  @Data(fields: [DataField<bool>('value')])
-  Exists,
-  @Data(fields: [DataField<String>('regex')])
-  Matches,
+// set the name to IncludeClass, as the name Include was conflicting with reference.dart enum in entry_queryable.dart
+// it also has an element named Include
+class QueryOperation {
+  final QueryOperationType operationType;
+  final dynamic value;
+
+  QueryOperation(this.operationType, this.value);
 }
